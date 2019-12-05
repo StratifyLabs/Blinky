1ff63392d3c5abc43ed2756e9ed70d6ae6f45166
Tyler <tyler.w.gilbert@gmail.com>
2019-12-04 20:05:46 -0700

    fixed windows cmake path

--- src/sl_config.h
+++ src/sl_config.h
@@ -9,6 +9,7 @@
 #define SL_CONFIG_VERSION_STRING "1.7"
 #define SL_CONFIG_VERSION_BCD 0x107
 #define SL_CONFIG_DOCUMENT_ID "8PtMwYjOghdeUpMxPmyl"
+#define SL_CONFIG_TEAM_ID ""
 #define SL_CONFIG_NAME "Blinky"
 #define SL_CONFIG_TYPE "app"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
